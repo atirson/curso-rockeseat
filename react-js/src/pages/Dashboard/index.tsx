dd1c047043b53711b3d8b90ce437d869dbf10f9b
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2020-10-13 21:39:50 -0300

    feat: Add element to list repositories.

--- react-js/src/pages/Dashboard/index.tsx
+++ react-js/src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,80 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="">
+          <img
+            src="https://avatars1.githubusercontent.com/u/45099648?s=460&u=b503a229f171a530bd33450bb834a763122dd4e7&v=4"
+            alt="My Profile"
+          />
+          <div>
+            <strong>atirson/gostack</strong>
+            <p>
+              O GoStack é um curso oferecido pela RocketSeat onde trazem 100%
+              prática e do jeito certo, ou seja as metodologias aplicadas são
+              totalmente alinhadas com que o mercado de trabalho exige. As
+              stacks são NodeJS, ReactJS e React Native.
+            </p>
+          </div>
+
+          <FiChevronRight size={80} />
+        </a>
+
+        <a href="">
+          <img
+            src="https://avatars1.githubusercontent.com/u/45099648?s=460&u=b503a229f171a530bd33450bb834a763122dd4e7&v=4"
+            alt="My Profile"
+          />
+          <div>
+            <strong>atirson/gostack</strong>
+            <p>
+              O GoStack é um curso oferecido pela RocketSeat onde trazem 100%
+              prática e do jeito certo, ou seja as metodologias aplicadas são
+              totalmente alinhadas com que o mercado de trabalho exige. As
+              stacks são NodeJS, ReactJS e React Native.
+            </p>
+          </div>
+
+          <FiChevronRight size={80} />
+        </a>
+
+        <a href="">
+          <img
+            src="https://avatars1.githubusercontent.com/u/45099648?s=460&u=b503a229f171a530bd33450bb834a763122dd4e7&v=4"
+            alt="My Profile"
+          />
+          <div>
+            <strong>atirson/gostack</strong>
+            <p>
+              O GoStack é um curso oferecido pela RocketSeat onde trazem 100%
+              prática e do jeito certo, ou seja as metodologias aplicadas são
+              totalmente alinhadas com que o mercado de trabalho exige. As
+              stacks são NodeJS, ReactJS e React Native.
+            </p>
+          </div>
+
+          <FiChevronRight size={80} />
+        </a>
+
+        <a href="">
+          <img
+            src="https://avatars1.githubusercontent.com/u/45099648?s=460&u=b503a229f171a530bd33450bb834a763122dd4e7&v=4"
+            alt="My Profile"
+          />
+          <div>
+            <strong>atirson/gostack</strong>
+            <p>
+              O GoStack é um curso oferecido pela RocketSeat onde trazem 100%
+              prática e do jeito certo, ou seja as metodologias aplicadas são
+              totalmente alinhadas com que o mercado de trabalho exige. As
+              stacks são NodeJS, ReactJS e React Native.
+            </p>
+          </div>
+
+          <FiChevronRight size={80} />
+        </a>
+      </Repositories>
     </>
   );
 };
--- react-js/src/pages/Dashboard/styles.ts
+++ react-js/src/pages/Dashboard/styles.ts
@@ -44,3 +44,55 @@ export const Form = styled.form`
     }
   }
 `;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.3s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+
+      svg {
+        margin-left: auto;
+        color: #cbcbd6;
+      }
+    }
+  }
+`;
